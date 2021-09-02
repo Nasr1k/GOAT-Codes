0dce7c9abf5e5713be5dcaaae9d7e21de8e6cf3f
Nasr1k <90006563+Nasr1k@users.noreply.github.com>
2021-09-02 16:37:22 -0400

    Add files via upload

---
+++ TheGreatestCodeEver.py
@@ -0,0 +1,4 @@
+loser = input('who is a loser?: ')
+print(loser, 'is a nerd ass jock mf')
+print(loser, 'will never recover')
+print(r'https://ca-times.brightspotcdn.com/dims4/default/3bdc647/2147483647/strip/true/crop/3200x1768+0+0/resize/840x464!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F7c%2F1d%2Fc11c1b0e46cab3e7d14be3663c1e%2Fla-et-mn-tiff-movies-02.jpg')
